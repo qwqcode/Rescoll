825a69b93a1e41568f6cba011db56837f999a959
qwqcode <1149527164@qq.com>
2018-12-13 21:57:26 +0800

    Window Header

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,38 @@
 <template>
-  <div id="app">
+  <div id="app" class="app">
+    <window-header></window-header>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import WindowHeader from './components/WindowHeader'
+
   export default {
-    name: 'rescoll'
+    name: 'rescoll',
+    components: { WindowHeader }
   }
 </script>
 
 <style>
-  /* CSS */
+* {
+    margin: 0;
+    padding: 0;
+    text-rendering: optimizeLegibility;
+    box-sizing: border-box;
+}
+
+body {
+  color: #fff;
+  -webkit-backface-visibility: hidden;
+}
+
+.app {
+    border: 1px solid rgb(51, 51, 51);
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    bottom: 0px;
+}
 </style>
---
+++ src/renderer/components/WindowHeader.vue
@@ -0,0 +1,128 @@
+<template>
+  <header class="header">
+    <div class="header-inner">
+      <div class="controls">
+        <div class="window-icon-bg" @click="minimize">
+          <div class="window-icon window-minimize"></div>
+        </div>
+        <div class="window-icon-bg" @click="maximize">
+          <div :class="{ 'window-icon': true, 'window-maximize': !this.isMaximized, 'window-unmaximize': this.isMaximized }"></div>
+        </div>
+        <div class="window-icon-bg red" @click="close">
+          <div class="window-icon window-close"></div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import { remote } from 'electron'
+
+let win = remote.getCurrentWindow()
+
+export default {
+  name: 'window-header',
+  data () {
+    return {
+      isMaximized: false
+    }
+  },
+  mounted () {
+    window.onresize = (e) => {
+      this.isMaximized = win.isMaximized()
+    }
+  },
+  methods: {
+    maximize () {
+      if (win.isMaximized()) {
+        win.unmaximize()
+      } else {
+        win.maximize()
+      }
+    },
+    minimize () {
+      win.minimize()
+    },
+    close () {
+      win.close()
+    }
+  },
+  computed: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  position: fixed;
+  top: 1px;
+  left: 1px;
+  right: 1px;
+  z-index: 100;
+  height: 34px;
+  width: 100%;
+  background: rgb(40, 44, 52);
+}
+
+.header-inner {
+  height: 34px;
+  width: 100%;
+  position: fixed;
+  top: 1px;
+  left: 1px;
+  right: 1px;
+  -webkit-app-region: drag;
+  user-select: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.controls {
+  display: flex;
+  width: 136px;
+  height: 34px;
+  justify-content: space-between;
+  position: fixed;
+  right: 0px;
+
+  .window-icon-bg {
+    display: inline-block;
+    -webkit-app-region: no-drag;
+    height: 100%;
+    width: 33.34%;
+
+    .window-icon {
+      height: 100%;
+      width: 100%;
+      -webkit-mask-size: 23.1%;
+      background-color: #9da5b4;
+
+      &.window-minimize {
+        -webkit-mask: url(~@/assets/icons/minimize.svg) no-repeat 50% 50%;
+      }
+
+      &.window-maximize {
+        -webkit-mask: url(~@/assets/icons/maximize.svg) no-repeat 50% 50%;
+      }
+
+      &.window-unmaximize {
+        -webkit-mask: url(~@/assets/icons/unmaximize.svg) no-repeat 50% 50%;
+      }
+
+      &.window-close {
+        -webkit-mask: url(~@/assets/icons/close.svg) no-repeat 50% 50%;
+      }
+    }
+
+    &:hover {
+      background-color: hsla(0,0%,100%,.1);
+    }
+
+    &.red:hover {
+      background-color: rgba(232,17,35,.9);
+    }
+  }
+}
+</style>
