6e808f422df900ef9a81f4d56357472469643510
qwqcode <1149527164@qq.com>
2019-08-02 17:17:03 +0800

    Notify component done.

---
+++ components/base/Notifications.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="notify-layer" />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class Notifications extends Vue {
+  mounted () {
+    Vue.prototype.$notify = new Notify(this.$el)
+  }
+}
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $notify: Notify
+  }
+}
+
+class Notify {
+  private layerEl: Element
+
+  constructor (layerEl: Element) {
+    this.layerEl = layerEl
+  }
+
+  add (message: string, level?: string, timeout: number = 1000): void {
+    window.console.log(`[notify][${level}][${new Date().toLocaleString()}] ${message}`)
+
+    const notifyElem = document.createElement('div')
+    notifyElem.className = `notify-item anim-fade-in ${(level ? 'type-' + level : '')}`
+    notifyElem.innerHTML = `<p class="notify-content">${message.replace('\n', '<br/>')}</p>`
+    this.layerEl.prepend(notifyElem)
+
+    const notifyRemove = function () {
+      notifyElem.className += ' anim-fade-out'
+      window.setTimeout(function () {
+        notifyElem.remove()
+      }, 200)
+    }
+
+    const timeOutFn = window.setTimeout(() => {
+      notifyRemove()
+    }, timeout)
+
+    notifyElem.onclick = () => {
+      window.clearTimeout(timeOutFn)
+      notifyRemove()
+    }
+  }
+
+  success (message: string) {
+    this.add(message, 's')
+  }
+
+  error (message: string) {
+    this.add(message, 'e')
+  }
+
+  warning (message: string) {
+    this.add(message, 'w')
+  }
+
+  info (message: string) {
+    this.add(message, 'i')
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.notify-layer {
+  $width: 600px;
+
+  position: fixed;
+  z-index: 9999;
+  top: 80px;
+  left: calc(50vw - (#{$width} / 2));
+  width: $width;
+  pointer-events: none;
+
+  /deep/ .notify-item {
+    display: block;
+    overflow: hidden;
+    background-color: #2c2c2c;
+    border-color: #2c2c2c;
+    color: #FFF;
+    cursor: pointer;
+    pointer-events: all;
+    box-shadow: 0 1px 4px rgba(25, 25, 25, 0.4);
+    border-radius: 2px;
+
+    &:not(:last-child) {
+      margin-bottom: 13px;
+    }
+
+    &.type-s {
+      color: #FFF;
+      background: rgba(87, 213, 159, 0.83);
+      box-shadow: 0 1px 4px rgba(87, 213, 159, 0.4);
+    }
+
+    &.type-e {
+      color: #FFF;
+      background: rgba(255, 111, 108, 0.83);
+      box-shadow: 0 1px 4px rgba(255, 111, 108, 0.4);
+    }
+
+    &.type-w {
+      color: #FFF;
+      background: rgba(255, 199, 33, 0.83);
+      box-shadow: 0 1px 4px rgba(255, 199, 33, 0.4);
+    }
+
+    &.type-i {
+      color: #FFF;
+      background: rgba(46, 188, 252, 0.83);
+      box-shadow: 0 1px 4px rgba(46, 188, 252, 0.4);
+    }
+
+    p {
+      line-height: 1.8;
+      padding: 5px 20px;
+      margin: 0;
+      text-align: center;
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,6 +6,7 @@
       <nuxt />
     </div>
     <SidebarLayer />
+    <Notifications />
   </div>
 </template>
 
@@ -14,9 +15,13 @@ import { Vue, Component } from 'nuxt-property-decorator'
 import Header from '../components/base/Header.vue'
 import NavSidebar from '../components/base/NavSidebar.vue'
 import SidebarLayer from '../components/base/Sidebars/_SidebarLayer.vue'
+import Notifications from '../components/base/Notifications.vue'
 
 @Component({
-  components: { Header, NavSidebar, SidebarLayer }
+  components: { Header, NavSidebar, SidebarLayer, Notifications }
 })
-export default class extends Vue {}
+export default class extends Vue {
+  mounted () {
+  }
+}
 </script>
